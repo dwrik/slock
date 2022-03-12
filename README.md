444cbdface636c70cbf20b48805caf421a024a56
Wrik Das <wrikdas22@gmail.com>
2022-03-12 17:36:33 +0530

    Added dpms, dwmlogo, caps and xresources pathes

---
+++ README.md
@@ -0,0 +1,33 @@
+# slock - simple screen locker
+
+simple screen locker utility for X.
+
+## dwrik's build
+
+This is my build of slock (simple screen locker) from (suckless)[https://suckless.org/]. This build includes the following patches:
+
+- dpms - to automatically turn off screen after given interval
+- dwmlogo - shows a custom shape (dwm logo by default) instead of a blank screen
+- capscolor - changes indicator color if caps lock is activated
+- xresources - to draw in colors from xresources (pywal compatible)
+
+## Requirements
+
+In order to build slock you need the Xlib header files.
+
+## Installation
+
+Edit config.mk to match your local setup (slock is installed into
+the /usr/local namespace by default).
+
+Afterwards enter the following command to build and install slock
+(if necessary as root):
+```
+make clean install
+```
+
+## Running slock
+
+Simply invoke the 'slock' command. To get out of it, enter your password.
+
+Slock can also be enabled as a systemd unit or be invoked with xss-lock during xinit.
--- config.def.h
+++ config.def.h
@@ -1,12 +1,46 @@
 /* user and group to drop privileges to */
 static const char *user  = "nobody";
-static const char *group = "nogroup";
+static const char *group = "nobody";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[BACKGROUND] = "black",     /* after initialization */
+	[INIT]       = "black",     /* after initialization */
+	[INPUT]      = "#005577",   /* during input */
+	[FAILED]     = "#CC3333",   /* wrong password */
+	[CAPS]       = "red",       /* CapsLock on */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[BACKGROUND] },
+		{ "color7",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "color3",       STRING,  &colorname[CAPS] },
+};
+
+/* insert grid pattern with scale 1:1, the size can be changed with logosize */
+static const int logosize = 55;
+static const int logow = 12;	/* grid width and height for right center alignment*/
+static const int logoh = 6;
+
+static XRectangle rectangles[9] = {
+	/* x	y	w	h */
+	{ 0,	3,	1,	3 },
+	{ 1,	3,	2,	1 },
+	{ 0,	5,	8,	1 },
+	{ 3,	0,	1,	5 },
+	{ 5,	3,	1,	2 },
+	{ 7,	3,	1,	2 },
+	{ 8,	3,	4,	1 },
+	{ 9,	4,	1,	2 },
+	{ 11,	4,	1,	2 },
+};
+
+/* time in seconds before the monitor shuts down */
+static const int monitortime = 20;
